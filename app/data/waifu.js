af4a34b71a2ada698d0bfa8db806947cf68c4fe3
Lonnie Jones <lonnie34j@gmail.com>
2016-10-20 02:04:25 -0400

    pics

--- app/data/waifu.js
+++ app/data/waifu.js
@@ -1,27 +1,27 @@
 
 var waifus = [{
 	name: "Rias",
-	image: "",
+	image: "https://lh3.googleusercontent.com/-GxK9YeFYha8/VX-SwHH3grI/AAAAAAAAC2w/HgS1JP8-qyE/w1920-h1080/Rias-Gremory-04.png",
 	scores: [5,3,4,5,5,2,3,5,1,1]
 }, {
 	name: "Yoko",
-	image: "",
+	image: "http://fastfrag.ru/uploads/imgs/pre_1440404720__ajzzjsly7ww.jpg",
 	scores: [5,1,2,1,1,2,5,5,4,1]
 }, {
 	name: "Rem",
-	image: "",
+	image: "http://i.imgur.com/sGwXxVx.jpg",
 	scores: [3,3,,1,3,5,1,2,2,2,1]
 }, {
 	name: "Camillia",
-	image: "",
+	image: "http://cdn.idigitaltimes.com/sites/idigitaltimes.com/files/styles/embed/public/2016/02/24/fire-emblem-fates-camilla-character.jpg",
 	scores: [3,5,5,4,4,1,2,5,1,]
 }, {
 	name: "Master Raven",
-	image: "",
+	image: "https://pbs.twimg.com/media/Cnr7dS0UMAQ3cp9.jpg",
 	scores: [2,1,2,3,2,1,1,4,2,3]
 }, {
 	name: "Ako", 
-	image: "",
+	image: "http://scontent.cdninstagram.com/t51.2885-15/e35/13725704_937661959676598_1921365451_n.jpg?ig_cache_key=MTMwNzg0NjQ4ODMzNjczOTg5Ng%3D%3D.2",
 	scores: [1,3,3,1,2,5,4,4,1,5]
 
 }];
