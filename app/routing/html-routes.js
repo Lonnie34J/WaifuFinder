c40a5d52d5d70b0c19fb20b0a8e28d8b02b50b74
Lonnie Jones <lonnie34j@gmail.com>
2016-10-19 23:15:39 -0400

    comment

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -1,11 +1,18 @@
 
+var path = require('path');
 
-app.use('/public', express.static('public'));
+module.exports = function (app) {
+	// HTML GET Requests
+	// Below code handles when users "visit" a page.
+	// In each of the below cases the user is shown an HTML page of content
+	// ---------------------------------------------------------------------------
 
-app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname, 'home.html'));
-});
+	app.get('/survey', function (req, res) {
+		res.sendFile(path.join(__dirname + '/../public/survey.html'));
+	});
 
-app.get('/survey', function (req, res) {
-	res.sendFile(path.join(__dirname, 'survey.html'));
-});
+	// If no matching route is found default to home
+	app.use(function (req, res) {
+		res.sendFile(path.join(__dirname + '/../public/home.html'));
+	});
+};
